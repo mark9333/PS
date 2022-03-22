a1d4bc2903767a58203b74694ac329393a3bc4e1
markm <markm@DESKTOP-OVFS7MG>
2022-03-22 19:58:07 +0200

    Add project files.

---
+++ StudentInfoSystem/StudentData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentInfoSystem
+{
+    class StudentData
+    {
+        static public List<Student> TestStudents { get; } = new List<Student>()
+        {
+            new Student("Ivan", "Ivanov", "Ivanov", "FKST", "KSI", "bachelor", "ОК", "121219***", 6, 1, 31)
+        };
+    }
+}
---
+++ WPFhello/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFhello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FirstButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.txtName.Text.Length > 2)
+            {
+                MessageBox.Show("Hello " + this.txtName.Text + "!");
+
+            }
+            else
+            {
+                MessageBox.Show("Put a longer name");
+
+            }
+        }
+    }
+}
